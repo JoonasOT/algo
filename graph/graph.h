666ab2c4bc4bf05001f5f1af325dcdc3bb5aab81
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-01-13 02:41:30 +0200

    started dijkstra

--- graph/graph.h
+++ graph/graph.h
@@ -1,6 +1,16 @@
 #include <vector.h>
 
 namespace graph {
+	enum COLOR { WHITE, GRAY, BLACK }; 
+
 	template<class T>
-	std::vector<const *T> 
+	struct dijkstra_wrapper {
+		T* data;
+		COLOR c = WHITE;
+		long int dist = -1;
+		dijkstra_wrapper* prev = nullptr;
+	};
+
+	template<class T, struct costF>
+	std::vector<dijkstra_wrapper> dijkstra(const std::vector<T>& vertecies, const size_t& start_i, costF& cost_func);
 }
