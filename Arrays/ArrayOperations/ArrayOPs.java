66cefefe88b0f172662393c1374d0a3bc5c58106
Joonas Tuominen <joonas.o.tuominen@outlook.com>
2024-01-29 00:07:39 +0200

    Update ArrayOPs.java

--- Arrays/ArrayOperations/ArrayOPs.java
+++ Arrays/ArrayOperations/ArrayOPs.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -10,5 +9,8 @@ public class ArrayOPs {
     }
     public static <T> ArrayList<Integer> get_frequency(ArrayList<T> arr, Stream<T> possible_elements) {
         return new ArrayList<>(possible_elements.mapToInt(elem -> Collections.frequency(arr, elem)).boxed().toList());
+    }
+    public static <T> ArrayList<T> get_values_from_X_with_indecies(ArrayList<T> X, IntStream indecies) {
+        return new ArrayList<>(indecies.mapToObj(X::get).toList());
     }
 }
